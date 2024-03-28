e84d1c34a1958d7664f935bab49ce73947dcdfed
TushigBro <411021383@gms.ndhu.edu.tw>
2024-03-28 22:01:20 +0800

    login screen initialize

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -10,6 +10,60 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text('Login Screen');
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 182),
+            const Text(
+              'Log in',
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            ),
+            const Text(
+              'Please log in to continue',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 60),
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.mail),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 5),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 100),
+            InkWell(
+              child: Container(
+                height: 57,
+                decoration: BoxDecoration(
+                  color: Color(0xff00AAFF),
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: Center(
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
