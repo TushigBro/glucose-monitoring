db74dcdf8641f0f60acda3f74b63b714dfd5dcbb
TushigBro <411021383@gms.ndhu.edu.tw>
2024-04-17 17:43:49 +0800

    small changes

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -15,78 +15,81 @@ class _LoginScreenState extends State<LoginScreen> {
   String password = ' ';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 182),
-                const Text(
-                  'Log in',
-                  style: TextStyle(fontSize: 32),
-                ),
-                const Text(
-                  'Please log in to continue',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w200),
-                ),
-                const SizedBox(height: 60),
-                TextField(
-                  onChanged: (value) {
-                    username = value;
-                  },
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.mail),
-                    hintText: 'username',
-                    hintStyle: TextStyle(fontWeight: FontWeight.w200),
-                    border: OutlineInputBorder(),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 182),
+                  const Text(
+                    'Log in',
+                    style: TextStyle(fontSize: 32),
                   ),
-                ),
-                const SizedBox(height: 5),
-                TextField(
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.lock),
-                      hintText: 'password',
+                  const Text(
+                    'Please log in to continue',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w200),
+                  ),
+                  const SizedBox(height: 60),
+                  TextField(
+                    onChanged: (value) {
+                      username = value;
+                    },
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.mail),
+                      hintText: 'username',
                       hintStyle: TextStyle(fontWeight: FontWeight.w200),
-                      border: OutlineInputBorder()),
-                ),
-                const SizedBox(height: 100),
-                InkWell(
-                  onTap: () {
-                    if (username == 'admin' && password == 'admin') {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const MainScreen(),
-                        ),
-                      );
-                    }
-                  },
-                  child: Container(
-                    height: 57,
-                    decoration: BoxDecoration(
-                      color: Color(0xff00AAFF),
-                      borderRadius: BorderRadius.circular(25.0),
+                      border: OutlineInputBorder(),
                     ),
-                    child: Center(
-                      child: Text(
-                        'Log In',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400),
+                  ),
+                  const SizedBox(height: 5),
+                  TextField(
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        hintText: 'password',
+                        hintStyle: TextStyle(fontWeight: FontWeight.w200),
+                        border: OutlineInputBorder()),
+                  ),
+                  const SizedBox(height: 100),
+                  InkWell(
+                    onTap: () {
+                      if (username == 'admin' && password == 'admin') {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const MainScreen(),
+                          ),
+                        );
+                      }
+                    },
+                    child: Container(
+                      height: 57,
+                      decoration: BoxDecoration(
+                        color: Color(0xff00AAFF),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Log In',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
--- lib/auth/register_screen.dart
+++ lib/auth/register_screen.dart
@@ -13,89 +13,92 @@ class _RegisterScreenState extends State<RegisterScreen> {
   String password = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 182),
-                const Text(
-                  'Sign up',
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w400),
-                ),
-                const Text(
-                  'Please sign up to continue',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w200),
-                ),
-                const SizedBox(height: 60),
-                TextField(
-                  onChanged: (value) {
-                    username = value;
-                  },
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.mail),
-                    hintText: 'username',
-                    hintStyle: TextStyle(fontWeight: FontWeight.w200),
-                    border: OutlineInputBorder(),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 182),
+                  const Text(
+                    'Sign up',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.w400),
                   ),
-                ),
-                const SizedBox(height: 5),
-                TextField(
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock),
-                    hintText: 'password',
-                    hintStyle: TextStyle(fontWeight: FontWeight.w200),
-                    border: OutlineInputBorder(),
+                  const Text(
+                    'Please sign up to continue',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w200),
                   ),
-                ),
-                const SizedBox(height: 5),
-                TextField(
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock),
-                    hintText: 'Confirm your password',
-                    hintStyle: TextStyle(fontWeight: FontWeight.w200),
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 70),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                InfoScreen(username: username)));
-                  },
-                  child: Container(
-                    height: 57,
-                    decoration: BoxDecoration(
-                      color: Color(0xff00AAFF),
-                      borderRadius: BorderRadius.circular(25.0),
+                  const SizedBox(height: 60),
+                  TextField(
+                    onChanged: (value) {
+                      username = value;
+                    },
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.mail),
+                      hintText: 'username',
+                      hintStyle: TextStyle(fontWeight: FontWeight.w200),
+                      border: OutlineInputBorder(),
                     ),
-                    child: Center(
-                      child: Text(
-                        'Sign up',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400),
+                  ),
+                  const SizedBox(height: 5),
+                  TextField(
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      hintText: 'password',
+                      hintStyle: TextStyle(fontWeight: FontWeight.w200),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  TextField(
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      hintText: 'Confirm your password',
+                      hintStyle: TextStyle(fontWeight: FontWeight.w200),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 70),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  InfoScreen(username: username)));
+                    },
+                    child: Container(
+                      height: 57,
+                      decoration: BoxDecoration(
+                        color: Color(0xff00AAFF),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
