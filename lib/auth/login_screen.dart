a575e985ee3f854363069ce323e02c5d1f42105c
TushigBro <411021383@gms.ndhu.edu.tw>
2025-03-11 15:38:20 +0800

    Login Screen changes

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -22,73 +22,81 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: const EdgeInsets.all(30.0),
           child: Expanded(
             child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 182),
-                  const Text(
-                    'Log in',
-                    style: TextStyle(fontSize: 32),
-                  ),
-                  const Text(
-                    'Please log in to continue',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w200),
-                  ),
-                  const SizedBox(height: 60),
-                  TextField(
-                    onChanged: (value) {
-                      username = value;
-                    },
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.mail),
-                      hintText: 'username',
-                      hintStyle: TextStyle(fontWeight: FontWeight.w200),
-                      border: OutlineInputBorder(),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 130),
+                    Image.asset(
+                      'assets/images/logo.png',
+                      height: 159,
+                      width: 113,
                     ),
-                  ),
-                  const SizedBox(height: 5),
-                  TextField(
-                    onChanged: (value) {
-                      password = value;
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        hintText: 'password',
+                    const Text(
+                      'gGauge',
+                      style: TextStyle(fontSize: 32),
+                    ),
+                    const Text(
+                      'Lets be healthy',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w200),
+                    ),
+                    const SizedBox(height: 90),
+                    TextField(
+                      onChanged: (value) {
+                        username = value;
+                      },
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.mail),
+                        hintText: 'Email address',
                         hintStyle: TextStyle(fontWeight: FontWeight.w200),
-                        border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(height: 100),
-                  InkWell(
-                    onTap: () {
-                      if (username == 'admin' && password == 'admin') {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const MainScreen(),
-                          ),
-                        );
-                      }
-                    },
-                    child: Container(
-                      height: 57,
-                      decoration: BoxDecoration(
-                        color: Color(0xff00AAFF),
-                        borderRadius: BorderRadius.circular(25.0),
+                        border: OutlineInputBorder(),
                       ),
-                      child: Center(
-                        child: Text(
-                          'Log In',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400),
+                    ),
+                    const SizedBox(height: 5),
+                    TextField(
+                      onChanged: (value) {
+                        password = value;
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          hintText: 'Password',
+                          hintStyle: TextStyle(fontWeight: FontWeight.w200),
+                          border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 100),
+                    InkWell(
+                      onTap: () {
+                        if (username == 'admin' && password == 'admin') {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const MainScreen(),
+                            ),
+                          );
+                        }
+                      },
+                      child: Container(
+                        height: 57,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF18786F),
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Log In',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
